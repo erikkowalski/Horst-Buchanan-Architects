1d1f4f6de81c1fa452f45cc0466c8a81dc41d346
erikkowalski <erikkowalski@mac.com>
2016-10-04 16:12:06 -0400

    Added loops for repeater fields

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,14 +1,40 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php // get_template_part('templates/entry-meta'); ?>
+      <h1 class="entry-title hidden-xs"><?php the_title(); ?></h1>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
-    <h3>Project Name</h3>
-    <h3>Project Team</h3>
+    <section class="project-info container-fluid">
+    <div class="description col-md-7">
+		 <h3><?php the_field('project_title') ?></h3>
+		 <p><?php the_field('project_description') ?></p>
+    </div>
+
+    <div class="project-type col-sm-6 col-md-2">
+		 <h3>Project Type</h3>
+
+		 <?php if( have_rows('project_type_list') ): ?>
+			 <ul>
+				 <?php while( have_rows('project_type_list') ): the_row(); ?>
+				 <li><p><?php the_sub_field('project_type'); ?></p></li>
+				 <?php endwhile; ?>
+			 </ul>
+		 <?php endif; ?>
+    </div>
+		 <div class="team col-sm-6 col-md-3">
+			 <h3>Project Team</h3>
+			 <?php if( have_rows('project_team') ): ?>
+			 <ul>
+				 <?php while( have_rows('project_team') ): the_row(); ?>
+				 <li><p><span><?php the_sub_field('role'); ?></span>&nbsp;&nbsp;<a target="_blank" href="<?php the_sub_field('team_member_url'); ?>"><?php the_sub_field('team_member'); ?></a></p></li>
+				 <?php endwhile; ?>
+			 </ul>
+			 <?php endif; ?>
+		 </div>
+	  </section>
+
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
