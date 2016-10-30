ca70538561f8503550bc408fdeb33e96430cdbcf
erikkowalski <erikkowalski@mac.com>
2016-10-30 14:58:07 -0400

    Added flexiable content field loop

--- page-portfolio.php
+++ page-portfolio.php
@@ -3,28 +3,53 @@
 <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<?php //Display 3 random Costar Boston News Posts
-$project_args = [
-	'post_type' => 'project',
-	'posts_per_page' => 3
 
-];
 
-$project = new WP_Query( $project_args );
-?>
 
-<aside  class="">
-	<?php while ( $project->have_posts() ) : $project->the_post(); ?>
-	<div class="col-md-6">
-		<?php if ( has_post_thumbnail() ) {
-	the_post_thumbnail();
-} ?>
-		<h4 class="entry-title"> <a href="<?php the_permalink(); ?>"><?php the_title();?> </a></h4>
-		<div class="entry-content">
-			<?php // the_excerpt();?>
+
+<?php if( have_rows('portfolio_grid') ): ?>
+
+	<?php while ( have_rows('portfolio_grid') ) : the_row(); ?>
+		<!-- Grid One -->
+		<?php	if( get_row_layout() == 'grid_one' ): ?>
+
+		<div class="row grid-one">
+			<div class="col-md-4">
+				<div class="grid-one-image-one">
+					<a href="<?php the_sub_field('grid_one_image_one_link'); ?>">
+						<img class="img-responsive" src="<?php	echo the_sub_field('grid_one_image_one');  ?>" alt="">
+					</a>
+				</div>
+				<div class="grid-one-image-two">
+					<a href="<?php the_sub_field('grid_one_image_two_link'); ?>">
+						<img class="img-responsive" src="<?php	echo the_sub_field('grid_one_image_two');  ?>" alt="">
+					</a>
+				</div>
+			</div>
+
+		 <div class="col-md-8">
+			 <div class="grid-one-image-three">
+				 <a href="<?php the_sub_field('grid_one_image_three_link'); ?>">
+					 <img class="img-responsive" src="<?php	echo the_sub_field('grid_one_image_three');  ?>" alt="">
+				 </a>
+			 </div>
+			 <div class="grid-one-image-four">
+				 <a href="<?php the_sub_field('grid_one_image_four_link'); ?>">
+					 <img class="img-responsive" src="<?php	echo the_sub_field('grid_one_image_four');  ?>" alt="">
+				 </a>
+			 </div>
+
+		 </div>
+	</div><!-- /.grid-one -->
+		<!-- Grid One -->
+	   <?php elseif( get_row_layout() == 'grid_two' ):  ?>
+	   <div class="row">
+			<div class="col-md-4">
+				<a href="<?php	the_sub_field('grid_two_image_one_link'); ?>"><img class="img-responsive" src="<?php	the_sub_field('grid_two_image_one'); ?>" alt=""></a>
+			</div>
 		</div>
-	</div>
-	<?php endwhile;
-	wp_reset_postdata();
-	?>
-</aside>
+	<?php	endif;
+		endwhile;
+		else :
+	// no layouts found
+endif; ?>
