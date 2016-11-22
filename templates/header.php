79bf84fbd324fbb487346216338d1ba8b2ee48df
erikkowalski <erikkowalski@mac.com>
2016-11-22 13:57:14 -0500

    changed container-fluid to container on navbar

--- templates/header.php
+++ templates/header.php
@@ -4,7 +4,7 @@
 ?>
 
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container-fluid">
+  <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
@@ -12,7 +12,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-		 <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><span>Horst&nbsp;</span>Buchanan<br><span class="sub-head" >ARCHITECTS</span> </a>
+		 <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><span>Horst&nbsp;</span>Buchanan </a>
     </div>
 
     <nav class="collapse navbar-collapse navbar-right" role="navigation">
