c9624b6e03f7d90aa61a77250b5ea5917d332ae5
erikkowalski <erikkowalski@mac.com>
2016-12-07 18:23:14 -0500

    Added arrows to image carousel

--- templates/content-single.php
+++ templates/content-single.php
@@ -34,7 +34,15 @@
 
 		  <?php endif; ?>
 
-
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+			  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			  <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+			  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			  <span class="sr-only">Next</span>
+		  </a>
 	  </div>
 
 
