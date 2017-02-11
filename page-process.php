9d56ddeec40d46b0e02c6b4f8e4ab4ccaad3ba48
erikkowalski <erikkowalski@mac.com>
2017-02-11 12:50:08 -0500

    added header to process page

--- page-process.php
+++ page-process.php
@@ -1,4 +1,9 @@
-<?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<section class="process-header-img">
+	<img class="img-responsive" src="<?php the_field('process_page_header') ?>" alt="">
+</section>
+<section class="process-content">
+	<?php while (have_posts()) : the_post(); ?>
+		<?php get_template_part('templates/page', 'header'); ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+	<?php endwhile; ?>
+</section>
