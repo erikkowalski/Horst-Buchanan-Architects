a1861b827d1f70448ef1b0233d283337b27c74c1
erikkowalski <erikkowalski@mac.com>
2017-02-11 18:36:04 -0500

    added contact markup

--- page-contact.php
+++ page-contact.php
@@ -1,3 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/content', 'page'); ?>
+<section class="contact-header-img">
+	<img class="img-responsive" src="<?php the_field('contact_header_image') ?>" alt="">
+
+	<div class="contact-info">
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>
+</section>
 <?php endwhile; ?>
