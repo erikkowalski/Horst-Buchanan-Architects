25797ccefea9cf82a32c367cb7a16b031957b3d5
erikkowalski <erikkowalski@mac.com>
2017-02-11 18:35:54 -0500

    added custom fields to about us page

--- page-about-us.php
+++ page-about-us.php
@@ -1,19 +1,26 @@
 <?php while (have_posts()) : the_post(); ?>
-<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
 
+<section class="about-us-content">
+	<?php get_template_part('templates/page', 'header'); ?>
+	<?php get_template_part('templates/content', 'page'); ?>
 <div class="about-mary row">
-<div class="col-md-4">
-		<img class="img-responsive" src="<?php the_field('mary_image') ?>" alt="Mary Horst headshot">
+<div class="col-md-5">
+		<img class="img-responsive profile-img" src="<?php the_field('mary_image') ?>" alt="Mary Horst headshot">
 	</div>
-	<h4><?php the_field('mary_title') ?></h4>
-	<p><?php the_field('mary_text') ?></p>
+	<div class="col-md-7">
+		<h4><?php the_field('mary_title') ?></h4>
+		<p><?php the_field('mary_text') ?></p>
+	</div>
 </div>
+<hr>
 <div class="about-david row">
-<div class="col-md-4">
-<img class="img-responsive" src="<?php the_field('david_image') ?>" alt="David Buchanan headshot">
+	<div class="col-md-5">
+		<img class="img-responsive profile-img" src="<?php the_field('david_image') ?>" alt="David Buchanan headshot">
 	</div>
-	<h4><?php the_field('david_title') ?></h4>
-	<p><?php the_field('david_text') ?></p>
+	<div class="col-md-7">
+		<h4><?php the_field('david_title') ?></h4>
+		<p><?php the_field('david_text') ?></p>
+	</div>
 </div>
+</section>
 <?php endwhile; ?>
