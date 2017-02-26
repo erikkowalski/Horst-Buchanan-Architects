bc0581fed77ff10599b9d1aa6d53661f8758ac78
erikkowalski <erikkowalski@mac.com>
2017-02-26 14:40:02 -0500

    added hero quote

--- page-approach.php
+++ page-approach.php
@@ -1,6 +1,8 @@
 <?php while (have_posts()) : the_post(); ?>
 <div class="approach-content">
 	<?php get_template_part('templates/page', 'header'); ?>
+	<h4><?php the_field('header_quote') ?></h4>
+	<hr>
 	<?php get_template_part('templates/content', 'page'); ?>
 </div>
 <?php endwhile; ?>
