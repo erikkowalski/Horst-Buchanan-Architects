1148cf6376f10013d659b87c7fdb5bbb293aeb72
erikkowalski <erikkowalski@mac.com>
2017-02-26 14:40:32 -0500

    moved header imge into content, removed sectiion.process-header-img

--- page-process.php
+++ page-process.php
@@ -1,9 +1,12 @@
-<section class="process-header-img">
-	<img class="img-responsive" src="<?php the_field('process_page_header') ?>" alt="">
-</section>
+
 <section class="process-content">
+
 	<?php while (have_posts()) : the_post(); ?>
 		<?php get_template_part('templates/page', 'header'); ?>
+		<h4><?php the_field('process_quote') ?></h4>
+		<hr>
+			<img class="img-responsive" src="<?php the_field('process_page_header') ?>" alt="">
+		<hr>
 		<?php get_template_part('templates/content', 'page'); ?>
 	<?php endwhile; ?>
 </section>
