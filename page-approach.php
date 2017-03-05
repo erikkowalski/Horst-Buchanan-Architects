f5b90ead4ab866da53d55816efa08b86bda5b83d
erikkowalski <erikkowalski@mac.com>
2017-03-05 17:27:40 -0500

    Addded approach header image

--- page-approach.php
+++ page-approach.php
@@ -3,6 +3,8 @@
 	<?php get_template_part('templates/page', 'header'); ?>
 	<h4><?php the_field('header_quote') ?></h4>
 	<hr>
+	<img class="img-responsive" src="<?php the_field('approach_header_image') ?>" alt="">
+	<hr>
 	<?php get_template_part('templates/content', 'page'); ?>
 </div>
 <?php endwhile; ?>
