8ff2f351b751af9daf85e12ac88cc808db13508b
erikkowalski <erikkowalski@mac.com>
2017-04-16 18:07:22 -0400

    removed less from build process

--- gulpfile.js
+++ gulpfile.js
@@ -85,9 +85,7 @@ var cssTasks = function(filename) {
     .pipe(function() {
       return gulpif(enabled.maps, sourcemaps.init());
     })
-    .pipe(function() {
-      return gulpif('*.less', less());
-    })
+
     .pipe(function() {
       return gulpif('*.scss', sass({
         outputStyle: 'nested', // libsass doesn't support expanded yet
