0cd72e779bf91167ebeea24be4b678eea28a342a
erikkowalski <erikkowalski@mac.com>
2017-04-16 11:36:52 -0400

    changed process page styling to match other layouts

--- page-process.php
+++ page-process.php
@@ -4,71 +4,45 @@
 		<?php get_template_part('templates/page', 'header'); ?>
 		<h4><?php the_field('process_quote') ?></h4>
 		<hr>
-
-	<div class="process-bottom-section row">
-	<div class="col-md-6">
-		<!-- Design Images Carousel -->
-		<div id="carousel-process-page" class="carousel fade" data-ride="carousel">
-			<?php $images = get_field('process_design_gallery');
-				if( $images ): ?>
-			<?php $dataId=0 ?>
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox">
-				<?php $slideId = 0; ?>
-				<?php foreach( $images as $image ): ?>
-				<div class="item " id="design_slide<?php echo $slideId;?>">
-					<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="  " />
-				</div>
-				<?php $slideId++; ?>
-				<?php endforeach; ?>
+	<!-- Design Images Carousel -->
+	<div id="carousel-process-page" class="carousel fade" data-ride="carousel" >
+		<?php $images = get_field('process_design_gallery');
+			if( $images ): ?>
+		<?php $dataId=0 ?>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<?php $slideId = 0; ?>
+			<?php foreach( $images as $image ): ?>
+			<div class="item " id="design_slide<?php echo $slideId;?>">
+				<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="  " />
 			</div>
-			<?php endif; ?>
+			<?php $slideId++; ?>
+			<?php endforeach; ?>
 		</div>
-		<hr>
-
-		<!-- Construction Images Carousel -->
-		<div id="carousel-process-page-construction" class="carousel fade" data-ride="carousel">
-				<?php $images = get_field('process_construction_gallery');
-				if( $images ): ?>
-				<?php $dataId=0 ?>
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox">
-				<?php $slideId = 0; ?>
-				<?php foreach( $images as $image ): ?>
-				<div class="item " id="construction_slide<?php echo $slideId;?>">
-					<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="  " />
-				</div>
-				<?php $slideId++; ?>
-				<?php endforeach; ?>
-			</div>
-			<?php endif; ?>
-		</div>
-		<hr>
-		<!-- Construction Images Carousel -->
-		<div id="carousel-process-page-administration" class="carousel fade" data-ride="carousel">
-			<?php $images = get_field('process_administration_gallery');
-				if( $images ): ?>
-				<?php $dataId=0 ?>
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox">
-				<?php $slideId = 0; ?>
-				<?php foreach( $images as $image ): ?>
-				<div class="item " id="administration_slide<?php echo $slideId;?>">
-					<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="  " />
-				</div>
-				<?php $slideId++; ?>
-				<?php endforeach; ?>
-			</div>
-			<?php endif; ?>
-		</div>
-		<hr>
-
-		</div> <!--/.col-md-6 -->
-
-		<div class="col-md-6">
-		<?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
-		</div><!--/.col-md-6 -->
+		<?php endif; ?>
 	</div>
+	<hr>
+	<div class="process-bottom-section ">
+	<div class="row">
+
+				<?php the_field('design_section_text') ?>
+		</div>
+		<div class="row">
+
+				<?php the_field('process_construction_text') ?>
+		</div><!-- /.col-md-6 -->
+
+			<div class="row">
+
+			<?php the_field('process_construction_administration_text') ?>
+		</div> <!-- .row -->
+		<hr>
+
+
+
+
+</div> <!--/.process-content -->
+		<?php endwhile; ?>
+
 
 </section>
